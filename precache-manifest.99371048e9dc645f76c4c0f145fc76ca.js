81e1ea24d9f4d1749fe5e825748fae730f265fc9
Marcin Zimecki <marcin.zimecki@lhsystems.com>
2020-04-30 13:22:57 +0200

    Updates

---
+++ precache-manifest.99371048e9dc645f76c4c0f145fc76ca.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b51f643dae81061d4a7075688912e8",
+    "url": "/MyReactTestRepo/index.html"
+  },
+  {
+    "revision": "aaae0e63d1578cc4649b",
+    "url": "/MyReactTestRepo/static/css/2.f5009910.chunk.css"
+  },
+  {
+    "revision": "ab5e1c970f22a61e13b3",
+    "url": "/MyReactTestRepo/static/css/main.5a2ee654.chunk.css"
+  },
+  {
+    "revision": "aaae0e63d1578cc4649b",
+    "url": "/MyReactTestRepo/static/js/2.b396a41b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyReactTestRepo/static/js/2.b396a41b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab5e1c970f22a61e13b3",
+    "url": "/MyReactTestRepo/static/js/main.5f53c695.chunk.js"
+  },
+  {
+    "revision": "47bf75db6af626f0d463",
+    "url": "/MyReactTestRepo/static/js/runtime-main.81a6e0c8.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/MyReactTestRepo/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/MyReactTestRepo/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/MyReactTestRepo/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/MyReactTestRepo/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/MyReactTestRepo/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/MyReactTestRepo/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/MyReactTestRepo/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/MyReactTestRepo/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/MyReactTestRepo/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/MyReactTestRepo/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/MyReactTestRepo/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/MyReactTestRepo/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/MyReactTestRepo/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/MyReactTestRepo/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/MyReactTestRepo/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/MyReactTestRepo/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
