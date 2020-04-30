58e9b45ce53c245dab74e18a16dcd8ed3a8f9180
Marcin Zimecki <marcin.zimecki@lhsystems.com>
2020-04-30 13:40:27 +0200

    removed comments

--- src/index.js
+++ src/index.js
@@ -204,53 +204,6 @@ class Users extends React.Component {
 
 }
 
-
-/*
-class Users extends React.Component {
-  constructor() {
-    super();    
-    
-    this.state = {
-      filteredUsers: allUsers
-    };
-  }
-
-  filterUsers(e) {
-    const text = e.currentTarget.value;
-    const filteredUsers = this.getFilteredUsersForText(text)
-    this.setState({
-      filteredUsers
-    })
-  }
-  
-  getFilteredUsersForText(text) {
-    return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
-  }
-  
-  render () {
-    return (
-      <div>
-        <input onInput={this.filterUsers.bind(this)} />
-        <UsersList users={this.state.filteredUsers} />
-      </div>
-    );
-  }
-};
-
-const UsersList = ({ users }) => {
-  if (users.length > 0) {
-    return (
-      <ul>
-        {users.map(user => <li key={user}>{user}</li>)}
-      </ul>
-    );
-  }
-
-  return (
-    <p>No results!</p>
-  );
-};
-*/
 function App() {
   return (
     <div>
