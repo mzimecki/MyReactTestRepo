88735ac72426c0548f01ec621e5337bbb93a5021
Marcin Zimecki <marcin.zimecki@gmail.com>
2020-05-01 14:40:40 +0200

    sample typeofweb course app with tests

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import UsersList from './UsersList'
+
+const allUsers = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania'];
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filteredUsers: allUsers
+    };
+  }
+
+  filterUsers = (e) => {
+    const text = e.currentTarget.value;
+    const filteredUsers = this.getFilteredUsersForText(text)
+    this.setState({
+      filteredUsers
+    })
+  }
+
+  getFilteredUsersForText(text) {
+    return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
+  }
+
+  render() {
+    return (
+      <div>
+        <input onInput={this.filterUsers} />
+        <UsersList users={this.state.filteredUsers} />
+      </div>
+    );
+  }
+};
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import UsersList from './UsersList'
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('includes input', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<input />)).toEqual(true)
+});
+
+it('includes UsersList', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<UsersList />)).toEqual(true)
+});
+
+it('passes all users to the UsersList', () => {
+  const app = shallow(<App />);
+  expect(app.find('UsersList').prop('users')).toEqual(['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania']);
+})
